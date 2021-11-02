d42c3d4d4a3a9f0f49e5be3f39ec528e0ee93f28
CodeMantra <rahmish@hotmail.com>
2021-11-01 21:02:05 -0400

    Initial Commit

---
+++ Recording/Maths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recording
+{
+    public class Maths
+    {
+        private readonly int num1;
+        private readonly int num2;
+
+        public Maths(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public int Add() => num1 + num2;
+
+        public int Subtract() => num1 - num2;
+
+        public int Multiply() => num1 * num2;
+
+        public int Divide() => num1 / num2;
+    }
+}
---
+++ Recording/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Recording
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 8;
+            int num2 = 4;
+
+            Maths maths = new Maths(num1, num2);
+
+            int add = maths.Add();
+            int subtract = maths.Subtract();
+            int multiply = maths.Multiply();
+            int divide = maths.Divide();
+
+            Console.WriteLine("Add => " + add.ToString());
+            Console.WriteLine("Subtract => " + subtract.ToString());
+            Console.WriteLine("Multiply => " + multiply.ToString());
+            Console.WriteLine("Divide => " + divide.ToString());
+
+            Console.WriteLine("All maths results.");
+            Console.ReadKey();
+        }
+    }
+}
